65d3a44ac706e68ccb48875d75212432b41c6d04
Ian Descôteaux <checksum0@protonmail.com>
2019-04-06 23:50:03 -0400

    Adding block headers subscription function

--- electrum/subscribe.go
+++ electrum/subscribe.go
@@ -1,7 +1,52 @@
 package electrum
 
+import "encoding/json"
+
 /* TODO:
- * - blockchain.headers.subscribe
  * - blockchain.scripthash.subscribe
  * - masternode.subscribe
  */
+
+// SubscribeHeadersResp ...
+type SubscribeHeadersResp struct {
+	Height int32  `json:"height"`
+	Hex    string `json:"hex"`
+}
+
+// SubscribeHeaders ...
+func (s *Server) SubscribeHeaders() (<-chan *SubscribeHeadersResp, error) {
+	resp := &struct {
+		Result *SubscribeHeadersResp `json:"result"`
+	}{}
+
+	err := s.request("blockchain.headers.subscribe", []interface{}{}, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respChan := make(chan *SubscribeHeadersResp, 1)
+	respChan <- resp.Result
+
+	go func() {
+		for msg := range s.listenPush("blockchain.headers.subscribe") {
+			if msg.err != nil {
+				return
+			}
+
+			resp := &struct {
+				Params []*SubscribeHeadersResp `json:"params"`
+			}{}
+
+			err := json.Unmarshal(msg.content, resp)
+			if err != nil {
+				return
+			}
+
+			for _, param := range resp.Params {
+				respChan <- param
+			}
+		}
+	}()
+
+	return respChan, nil
+}
